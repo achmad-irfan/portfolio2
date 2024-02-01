12701cd7bcc1102a90ed15715de0e4eadd3010db
achmad-irfan <achmad.irfan1@yahoo.com>
2024-02-01 15:08:14 +0700

    mm7

--- PORTFOLIO/urls.py
+++ PORTFOLIO/urls.py
@@ -19,11 +19,11 @@ from django.urls import path, include, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-# from projects.views import ProyekViewSet
-# from rest_framework import routers
+from projects.views import ProyekViewSet
+from rest_framework import routers
 
-# router = routers.DefaultRouter()
-# router.register('proyek', ProyekViewSet)
+router = routers.DefaultRouter()
+router.register('proyek', ProyekViewSet)
 
 app_name = 'app_home'
 urlpatterns = [
@@ -35,8 +35,8 @@ urlpatterns = [
     path('services/', include('services.urls')),
     path('contact/', include('contact.urls')),
     path("admin/", admin.site.urls),
-    # path('API/', include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('API/', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
 
 if settings.DEBUG:
