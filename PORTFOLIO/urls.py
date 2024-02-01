b5d4b9449355d86a423a6c0c6e3ff823844aa327
achmad-irfan <achmad.irfan1@yahoo.com>
2024-02-01 15:02:40 +0700

    mm3

--- PORTFOLIO/urls.py
+++ PORTFOLIO/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include, re_path
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
-from projects.views import ProyekViewSet
+# from projects.views import ProyekViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter()
