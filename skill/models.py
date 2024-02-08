adc820f2e228a1cedeed1553ce15bc0b6836cf45
achmad-irfan <achmad.irfan1@yahoo.com>
2024-02-08 11:22:32 +0700

    skill2

--- skill/models.py
+++ skill/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 
 class skill(models.Model):
-    skill_name = models.CharField(max_length=50)
+    skill_name = models.TextField(max_length=50)
 
     def __str__(self):
         return "{}".format(self.skill_name)
