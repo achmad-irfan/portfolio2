151e03beb6a734957cc757c614af736b5d75111e
achmad-irfan <achmad.irfan1@yahoo.com>
2024-02-10 23:20:17 +0700

    skill image

--- skill/models.py
+++ skill/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class skill(models.Model):
     skill_name = models.TextField(max_length=50)
+    iconSkill = models.ImageField(upload_to='media')
 
     def __str__(self):
         return "{}".format(self.skill_name)
